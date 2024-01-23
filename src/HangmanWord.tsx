609824d54f1567957b98a35c6d18cc60f412b167
casafurix <agnibha10@gmail.com>
2024-01-24 02:09:44 +0530

    gh-pages 2

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -6,7 +6,7 @@ type HangmanWordProps = {
 
 export function HangmanWord({reveal=false, guessedLetters, wordToGuess}: HangmanWordProps){
     return <div style={{display: "flex", gap: ".25em", fontSize: "6rem", fontWeight: "bold", textTransform: "uppercase", fontFamily: "monospace"}}>
-        {wordToGuess.split("").map((letter, index) => (
+        {wordToGuess.split("").map((letter) => (
             <span style={{borderBottom: ".1em solid black"}}>
                 <span style={{visibility: guessedLetters.includes(letter) || reveal ? "visible" : "hidden", color: !guessedLetters.includes(letter) && reveal ? "red" : "black"}}>{letter}</span>
             </span>
